108adbfcebf29397a472e1097b83bbfc16c09a39
mattdevio <magreenberg@fullsail.edu>
2023-11-02 23:44:15 -0600

    feat: longest palindrome with windows

--- src/longest_palindromic_substring.rs
+++ src/longest_palindromic_substring.rs
@@ -1,10 +1,30 @@
+#[allow(unused)]
 /// # Longest Palindromic Substring
 ///
 /// Give a string `s`, return the longest palindromic substring in `s`
 ///
 /// A string is palindromic if it reads the same forward and backward.
 pub fn longest_palindrom(s: String) -> String {
-    todo!()
+    fn is_palidrome(s: &[u8]) -> bool {
+        s.iter().zip(s.iter().rev()).all(|(l, r)| l == r)
+    }
+
+    for size in (1..=s.len()).rev() {
+        let win = s.as_bytes().windows(size);
+        win.for_each(|w| {
+            println!("for {} -> {:?}", &s, w);
+        });
+        match s
+            .as_bytes()
+            .windows(size)
+            .find(|substr| is_palidrome(substr))
+        {
+            Some(pal) => return String::from_utf8(pal.to_vec()).unwrap(),
+            None => continue,
+        }
+    }
+
+    String::from("")
 }
 
 #[cfg(test)]
