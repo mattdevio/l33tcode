7bedfde52e87efa1a71e661e1195bb2f733abf21
matzxrr <matzxrr@users.noreply.github.com>
2023-11-29 22:27:58 -0700

    feat: rename module to match question title

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@
 //! Not affiliated with leetcode.
 
 pub mod add_two_numbers;
-pub mod is_palindrome;
 pub mod longest_palindromic_substring;
 pub mod longest_substring_without_repeating_characters;
 pub mod median_of_two_sorted_arrays;
+pub mod palindrome_number;
 pub mod two_sum;
