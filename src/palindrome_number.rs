d4acc515bac895d2e6b51d89b70bc3da9c11453a
matzxrr <matzxrr@users.noreply.github.com>
2023-11-30 23:33:19 -0700

    feat: add chat gpt explaination

--- src/palindrome_number.rs
+++ src/palindrome_number.rs
@@ -4,6 +4,14 @@
 /// Given an integer `x`, return `true` if `x` is a palindrome.
 ///
 /// A integer is palindromic if its reads the same forward and backwards.
+// Chat GPT explanation:
+// In this example, the reverse_number function takes an integer as input and
+// uses a while loop to extract the last digit of the number in each iteration,
+// appending it to the reversed_number variable.
+//     The original number is then updated by removing its last digit
+//     (using integer division by 10).
+//     This process continues until the original number becomes zero.
+
 pub fn is_palindrome(x: i32) -> bool {
     let mut acc = x;
     let mut y = 0;
